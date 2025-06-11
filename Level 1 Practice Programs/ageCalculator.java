7b9f83c548b7276d0355f0525469e62866402de4
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-06-11 11:40:58 +0530

    adding ageCalculator, miles and average files

---
+++ Level 1 Practice Programs/ageCalculator.java
@@ -0,0 +1,7 @@
+public class ageCalculator{
+	public static void main(String[] args){
+	int birthYear = 2000;
+	System.out.println("Harry's age is in 2024 is " + (2024-birthYear));
+
+	}
+} 
---
+++ Level 1 Practice Programs/average.java
@@ -0,0 +1,10 @@
+public class average{
+	public static void main(String[] args){
+	double Maths = 94;
+	double Physics = 95;
+	double Chemistry = 96;
+	double average = (Maths + Physics + Chemistry) / 3;
+	System.out.println("Sam's average mark in PCM is " + average);
+
+	}
+}
