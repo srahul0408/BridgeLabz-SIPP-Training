a3c3c7b93423cda077b391be8563a0c46eec5ba1
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-06-14 10:23:59 +0530

    [Rahul] Add. count down the number using for-loop

---
+++ Control_Flow/Level_1/countDownUsingForLoop.java
@@ -0,0 +1,18 @@
+package Level_1;
+
+import java.util.Scanner;
+
+public class countDownUsingForLoop {
+
+	public static void main(String[] args) {
+		// Taking the number input from the user 
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter the value: ");
+		int counter = input.nextInt();
+		for(int i = counter; i > 0; i--) {
+			System.out.println(i);
+		}
+
+	}
+
+}
