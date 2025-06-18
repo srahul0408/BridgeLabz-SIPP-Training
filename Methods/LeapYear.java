9e33b6cf86483f2ec5825b5b846299af04f8b742
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-06-18 09:58:13 +0530

    [Rahul] Add. checking given year is leap year or not

---
+++ Methods/LeapYear.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class LeapYear {
+    // Method to check if a year is a leap year
+    static boolean isLeapYear(int year) {
+        if (year < 1582) {
+            return false;
+        }
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a year (should be 1582 or greater): ");
+        int year = sc.nextInt();
+
+        if (isLeapYear(year)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is not a Leap Year.");
+        }
+    }
+}
