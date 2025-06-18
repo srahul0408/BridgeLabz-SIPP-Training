d619697dceb6d7d2b0bc4c360681275396a56c0e
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-06-18 09:55:34 +0530

    [Rahul] Add. comparing the height and age among three friends

---
+++ Methods/FriendsChecker.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+public class FriendsChecker {
+    // Method to find the index of the youngest friend
+    public static int findYoungest(int[] ages) {
+        int minAge = ages[0];
+        int index = 0;
+        for (int i = 1; i < ages.length; i++) {
+            if (ages[i] < minAge) {
+                minAge = ages[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    // Method to find the index of the tallest friend
+    public static int findTallest(double[] heights) {
+        double maxHeight = heights[0];
+        int index = 0;
+        for (int i = 1; i < heights.length; i++) {
+            if (heights[i] > maxHeight) {
+                maxHeight = heights[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String[] names = {"Amar", "Akbar", "Anthony"};
+        int[] ages = new int[3];
+        double[] heights = new double[3];
+        // Get age and height input
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter age of " + names[i] + ": ");
+            ages[i] = sc.nextInt();
+            System.out.print("Enter height (in meters) of " + names[i] + ": ");
+            heights[i] = sc.nextDouble();
+        }
+        int youngestIndex = findYoungest(ages);
+        int tallestIndex = findTallest(heights);
+        System.out.println("The youngest friend is " + names[youngestIndex] + " with age " + ages[youngestIndex]);
+        System.out.println("The tallest friend is " + names[tallestIndex] + " with height " + heights[tallestIndex] + " meters");
+    }
+}
