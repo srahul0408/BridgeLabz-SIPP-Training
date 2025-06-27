cf20c0057e6f02868fb05b60003e1b7cb50b0e9c
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-06-27 20:14:30 +0530

    [Rahul] Add. checking prime number

---
+++ Strings/Built-InFunctions/PrimeChecker.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class PrimeChecker {
+
+    public static void main(String[] args) {
+        int number = getInput("Enter a number to check if it's prime: ");
+        
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+    }
+
+    public static int getInput(String prompt) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(prompt);
+        return scanner.nextInt();
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) return false;
+        if (n == 2) return true;
+        if (n % 2 == 0) return false;
+
+        for (int i = 3; i <= Math.sqrt(n); i += 2) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
