217f2bb5734ae5a8921e632cafd83ca5f4fe1074
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-06-28 17:47:18 +0530

    [Rahul] Add. University management system

---
+++ ConstructorsAndAccessModifiers/AccessModifiers/Student.java
@@ -0,0 +1,29 @@
+public class Student {
+    public int rollNumber;
+    protected String name;
+    private double CGPA;
+
+    public Student(int rollNumber, String name, double CGPA) {
+        this.rollNumber = rollNumber;
+        this.name = name;
+        this.CGPA = CGPA;
+    }
+
+    public double getCGPA() {
+        return CGPA;
+    }
+
+    public void setCGPA(double CGPA) {
+        this.CGPA = CGPA;
+    }
+}
+
+class PostgraduateStudent extends Student {
+    public PostgraduateStudent(int roll, String name, double cgpa) {
+        super(roll, name, cgpa);
+    }
+
+    public void displayInfo() {
+        System.out.println("Name: " + name); // accessing protected member
+    }
+}
