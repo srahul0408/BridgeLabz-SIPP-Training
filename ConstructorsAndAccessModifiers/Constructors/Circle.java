dc12cf43fa8a0b7f5e2bbedab2555a39fe2d3dad
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-06-28 17:51:40 +0530

    [Rahul] Add. initializing radius of a circle

---
+++ ConstructorsAndAccessModifiers/Constructors/Circle.java
@@ -0,0 +1,11 @@
+public class Circle {
+    private double radius;
+
+    public Circle() {
+        this(1.0); // Default radius
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+}
