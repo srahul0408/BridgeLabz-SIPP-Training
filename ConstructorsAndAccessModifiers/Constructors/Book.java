83c2039ba352e86fa9aaeecff47b8497652ba648
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-06-28 17:49:07 +0530

    [Rahul] Add. Book

---
+++ ConstructorsAndAccessModifiers/Constructors/Book.java
@@ -0,0 +1,15 @@
+public class Book {
+    private String title;
+    private String author;
+    private double price;
+
+    public Book() {
+        this("Unknown", "Unknown", 0.0);
+    }
+
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+}
