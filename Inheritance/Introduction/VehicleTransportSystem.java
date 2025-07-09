1a15cd290505bb2e858d4361c350aca97cc9f75e
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-07-09 09:18:18 +0530

    [Rahul] Add. Vehicle Transport System

---
+++ Inheritance/Introduction/VehicleTransportSystem.java
@@ -0,0 +1,79 @@
+// Superclass
+class Vehicle {
+    int maxSpeed;
+    String fuelType;
+
+    Vehicle(int maxSpeed, String fuelType) {
+        this.maxSpeed = maxSpeed;
+        this.fuelType = fuelType;
+    }
+
+    void displayInfo() {
+        System.out.println("Max Speed: " + maxSpeed + " km/h");
+        System.out.println("Fuel Type: " + fuelType);
+    }
+}
+
+// Subclass: Car
+class Car extends Vehicle {
+    int seatCapacity;
+
+    Car(int maxSpeed, String fuelType, int seatCapacity) {
+        super(maxSpeed, fuelType);
+        this.seatCapacity = seatCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Seat Capacity: " + seatCapacity);
+    }
+}
+
+// Subclass: Truck
+class Truck extends Vehicle {
+    double loadCapacity;
+
+    Truck(int maxSpeed, String fuelType, double loadCapacity) {
+        super(maxSpeed, fuelType);
+        this.loadCapacity = loadCapacity;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Load Capacity: " + loadCapacity + " tons");
+    }
+}
+
+// Subclass: Motorcycle
+class Motorcycle extends Vehicle {
+    boolean hasSidecar;
+
+    Motorcycle(int maxSpeed, String fuelType, boolean hasSidecar) {
+        super(maxSpeed, fuelType);
+        this.hasSidecar = hasSidecar;
+    }
+
+    @Override
+    void displayInfo() {
+        super.displayInfo();
+        System.out.println("Has Sidecar: " + (hasSidecar ? "Yes" : "No"));
+    }
+}
+
+// Main class to test
+public class VehicleTransportSystem {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = new Vehicle[3];
+        vehicles[0] = new Car(180, "Petrol", 5);
+        vehicles[1] = new Truck(120, "Diesel", 10.5);
+        vehicles[2] = new Motorcycle(150, "Petrol", true);
+
+        System.out.println("--- Vehicle Details ---");
+        for (Vehicle v : vehicles) {
+            v.displayInfo();
+            System.out.println();
+        }
+    }
+}
