e93ad385025cca264f3461b84f570182a5d2e13e
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-07-09 09:36:18 +0530

    [Rahul] Add. Smart home system

---
+++ Inheritance/SingleInheritance/SmartHomeSystem.java
@@ -0,0 +1,41 @@
+// Superclass: Device
+class Device {
+    String deviceId;
+    String status;
+
+    Device(String deviceId, String status) {
+        this.deviceId = deviceId;
+        this.status = status;
+    }
+
+    void displayStatus() {
+        System.out.println("Device ID: " + deviceId);
+        System.out.println("Status: " + status);
+    }
+}
+
+// Subclass: Thermostat
+class Thermostat extends Device {
+    double temperatureSetting;
+
+    Thermostat(String deviceId, String status, double temperatureSetting) {
+        super(deviceId, status);
+        this.temperatureSetting = temperatureSetting;
+    }
+
+    @Override
+    void displayStatus() {
+        super.displayStatus();
+        System.out.println("Temperature Setting: " + temperatureSetting + "°C");
+    }
+}
+
+// Main class to test
+public class SmartHomeSystem {
+    public static void main(String[] args) {
+        Thermostat thermostat = new Thermostat("TH-001", "ON", 22.5);
+
+        System.out.println("--- Device Status ---");
+        thermostat.displayStatus();
+    }
+}
