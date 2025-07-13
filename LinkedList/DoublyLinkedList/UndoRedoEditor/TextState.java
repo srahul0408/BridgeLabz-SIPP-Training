31e1f40788292349a8e9634ebef3daa7c88e5091
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-07-13 14:11:52 +0530

    [Rahul] Add. creating TextState class

---
+++ LinkedList/DoublyLinkedList/UndoRedoEditor/TextState.java
@@ -0,0 +1,13 @@
+package com.linkedlist.doublylinkedlist.undo_redofunctionalityfortexteditor;
+
+class TextState {
+    String content;
+    TextState prev;
+    TextState next;
+
+    public TextState(String content) {
+        this.content = content;
+        this.prev = null;
+        this.next = null;
+    }
+}
