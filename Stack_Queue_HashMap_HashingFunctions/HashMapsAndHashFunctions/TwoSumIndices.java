4e7a47c3042d21d7dc50ab0a3f768819b75d8a08
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-07-13 14:52:09 +0530

    [Rahul] Add. creating TwoSumIndices class

---
+++ Stack_Queue_HashMap_HashingFunctions/HashMapsAndHashFunctions/TwoSumIndices.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class TwoSumIndices {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        System.out.print("Enter target sum: ");
+        int target = sc.nextInt();
+
+        int[] result = twoSum(arr, target);
+        if (result != null) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No such pair found.");
+        }
+        sc.close();
+    }
+
+    static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
+            }
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+}
