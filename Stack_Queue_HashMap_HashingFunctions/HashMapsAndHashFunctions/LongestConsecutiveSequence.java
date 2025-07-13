87781a8f4cda5bcd22ca52f550c7388614789bed
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-07-13 14:54:08 +0530

    [Rahul] Add. creating LongestConsecutiveSequence class

---
+++ Stack_Queue_HashMap_HashingFunctions/HashMapsAndHashFunctions/LongestConsecutiveSequence.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class LongestConsecutiveSequence {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array: ");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        System.out.println("Enter array elements:");
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+
+        int longest = findLongestConsecutive(arr);
+        System.out.println("Length of longest consecutive sequence: " + longest);
+        sc.close();
+    }
+
+    static int findLongestConsecutive(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) set.add(num);
+
+        int longest = 0;
+        for (int num : set) {
+            if (!set.contains(num - 1)) {
+                int currentNum = num;
+                int count = 1;
+                while (set.contains(currentNum + 1)) {
+                    currentNum++;
+                    count++;
+                }
+                longest = Math.max(longest, count);
+            }
+        }
+        return longest;
+    }
+}
