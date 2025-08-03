e6d0d73a4d7d891e4dea4c4504a3157e7cd2cf20
srahul0408 <rahul.sahani_cs22@gla.ac.in>
2025-08-03 22:06:58 +0530

    [Rahul] Add. smartwarehousemanagementsystem file

---
+++ Generics/smartwarehousemanagementsystem.java
@@ -0,0 +1,69 @@
+abstract class WarehouseItem {
+    private String name;
+
+    public WarehouseItem(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract String getCategory();
+}
+
+class Electronics extends WarehouseItem {
+    public Electronics(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Electronics";
+    }
+}
+
+class Furniture extends WarehouseItem {
+    public Furniture(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Furniture";
+    }
+}
+
+class Groceries extends WarehouseItem {
+    public Groceries(String name) {
+        super(name);
+    }
+
+    @Override
+    public String getCategory() {
+        return "Groceries";
+    }
+}
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Storage<T extends WarehouseItem> {
+    private List<T> items = new ArrayList<>();
+
+    public void addItem(T item) {
+        items.add(item);
+    }
+
+    public List<T> getItems() {
+        return items;
+    }
+}
+
+class WarehouseUtility {
+    public static void displayItems(List<? extends WarehouseItem> items) {
+        for (WarehouseItem item : items) {
+            System.out.println(item.getCategory() + ": " + item.getName());
+        }
+    }
+}
